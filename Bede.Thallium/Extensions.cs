f0c339c43cff3a31614e67c9ad92639886f34185
Nicholas Little <nicholas.little@bedegaming.com>
2016-04-01 23:43:52 +0100

    thallium: Refactor extensions a little bit
    This commit removes the IsBadRequest extension and swaps it for
    IsClientError and IsServerError on the HttpStatusCode type.

--- Bede.Thallium/Extensions.cs
+++ Bede.Thallium/Extensions.cs
@@ -21,17 +21,6 @@ namespace Bede.Thallium
         }
 
         /// <summary>
-        /// Checks if the status code from a request exception
-        /// was a bad request
-        /// </summary>
-        /// <param name="ex"></param>
-        /// <returns></returns>
-        public static bool IsBadRequest(this HttpRequestException ex)
-        {
-            return HttpStatusCode.BadRequest == Code(ex);
-        }
-
-        /// <summary>
         /// Gets the content from a request exception
         /// </summary>
         /// <param name="ex"></param>
@@ -41,6 +30,26 @@ namespace Bede.Thallium
             return ex.Data.Contains(_content) ? (string) ex.Data[_content] : null;
         }
 
+        /// <summary>
+        /// Checks if a status code is in the client error range, i.e. 400 &lt;= x &lt; 500
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public static bool IsClientError(this HttpStatusCode code)
+        {
+            var c = (int) code;
+            return 400 <= c && c < 500;
+        }
+
+        /// <summary>
+        /// Checks if a status code is in the server error range, i.e. 500 &lt;= x
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public static bool IsServerError(this HttpStatusCode code)
+        {
+            return 500 <= (int) code;
+        }
 
         /// <summary>
         /// Check if a status code represents a successful outcome
