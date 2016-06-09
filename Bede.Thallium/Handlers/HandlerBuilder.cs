2e9f3795d357dacf80fc340ccb9caacd206d368c
Nicholas Little <nicholas.little@bedegaming.com>
2016-06-09 12:29:44 +0100

    thallium: Recording Handler
    This commit adds an event based handler and some fluent methods for
    building a handler pipeline using the variously available handlers, in
    addition we reset the base class of the ThrowOnFail handler so that it
    doesn't have to be the main handler in the pieline.

---
+++ Bede.Thallium/Handlers/HandlerBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+
+#pragma warning disable 1591
+
+namespace Bede.Thallium.Handlers
+{
+    public static class HandlerBuilder
+    {
+        public static ThrowOnFail ThrowOnFail(this HttpMessageHandler @this)
+        {
+            return new ThrowOnFail(@this);
+        }
+
+        public static ThrowOnFail ThrowOnFail(this ThrowOnFail @this)
+        {
+            return @this;
+        }
+
+        public static RecordingHandler Record(this HttpMessageHandler @this)
+        {
+            return new RecordingHandler(@this);
+        }
+
+        public static RecordingHandler Record(this RecordingHandler @this)
+        {
+            return @this;
+        }
+
+        public static RecordingHandler OnRequest(this RecordingHandler @this, EventHandler<HttpRequestMessage> handler)
+        {
+            @this.Request += handler;
+
+            return @this;
+        }
+
+        public static RecordingHandler OnResponse(this RecordingHandler @this, EventHandler<HttpResponseMessage> handler)
+        {
+            @this.Response += handler;
+
+            return @this;
+        }
+    }
+}
