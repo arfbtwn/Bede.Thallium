433e078d792312dbdeb1b6a0c0d651f3b018e338
Nicholas Little <nicholas.little@bedegaming.com>
2016-08-01 13:53:35 +0100

    factory: Reel in the generics
    The factory was very generic, too generic. This goes some of the way to
    reducing that which makes the re-implemented Auto method a lot simpler.

--- Bede.Thallium/Belt/Builder.cs
+++ Bede.Thallium/Belt/Builder.cs
@@ -1,16 +1,77 @@
-﻿namespace Bede.Thallium.Belt
+﻿using System;
+
+namespace Bede.Thallium.Belt
 {
+    /// <summary>
+    /// A base type for generic builder classes, implementing
+    /// the runtime builder interface and returning default(T)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Builder<T> : IBuilder<T>
+    {
+        readonly bool _empty;
+        readonly T    _value;
+
+        /// <summary>
+        /// Construct a builder returning the default
+        /// value for type T
+        /// </summary>
+        public Builder() { _empty = true; }
+
+        /// <summary>
+        /// Construct a builder returning a specific
+        /// value for type T
+        /// </summary>
+        /// <param name="value"></param>
+        public Builder(T value) { _value = value; }
+
+        Type   IBuilder.Type     => typeof(T);
+        object IBuilder.Build()  => Build();
+
+        /// <summary>
+        /// Builds the object
+        /// </summary>
+        /// <returns></returns>
+        public virtual T Build() => _value;
+
+        /// <summary>
+        /// Returns the value of the thing
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return _empty ? typeof(T).ToString() : _value?.ToString() ?? "null";
+        }
+    }
+
+    /// <summary>
+    /// Marks a type that builds an object
+    /// </summary>
+    public interface IBuilder
+    {
+        /// <summary>
+        /// The type the builder will build
+        /// </summary>
+        Type Type { get; }
+
+        /// <summary>
+        /// Build the object
+        /// </summary>
+        /// <returns></returns>
+        object Build();
+    }
+
     /// <summary>
     /// Marks a type that creates another type
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public interface IBuilder<out T>
+    public interface IBuilder<out T> : IBuilder
     {
         /// <summary>
         /// Create the type
         /// </summary>
         /// <returns></returns>
-        T Build();
+        new T Build();
     }
 
     /// <summary>
