a242e41d291c733718fcccf0e17628a1f09ce9cd
Nicholas Little <nicholas.little@bedegaming.com>
2016-09-02 11:34:26 +0100

    handlers: Alternative building strategy
    This commit adds some new handler types using Polly 4.3's new typed
    result handling setup allowing us to avoid using exceptions for
    messaging.

--- Bede.Thallium.Polly/PolicyBuilder.cs
+++ Bede.Thallium.Polly/PolicyBuilder.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 
 #pragma warning disable 1591
@@ -15,7 +17,7 @@ namespace Bede.Thallium.Polly
 
         static readonly TimeSpan OneMinute = TimeSpan.FromMinutes(1);
 
-        static TimeSpan Exponential(int attempt) => TimeSpan.FromMilliseconds(125 * Math.Pow(2, attempt));
+        static TimeSpan Exponential(int attempt) => TimeSpan.FromMilliseconds(125 * (Math.Pow(2, attempt) - 1));
 
         static bool IsUnknown(Exception e) => true;
 
@@ -23,6 +25,7 @@ namespace Bede.Thallium.Polly
 
         static bool IsServer(HttpRequestException e) => e.Code().IsServerError();
 
+        [Obsolete]
         public static RetryHandler<E> WaitAndRetry<E>(this Handler       @this,
                                                            Func<E, bool>  predicate,
                                                            int?           attempts  = null,
@@ -32,6 +35,7 @@ namespace Bede.Thallium.Polly
             return new RetryHandler<E>(@this, predicate, attempts ?? Three, backoff ?? Exponential);
         }
 
+        [Obsolete]
         public static CircuitBreakHandler<E> CircuitBreak<E>(this Handler       @this,
                                                                   Func<E, bool>  predicate,
                                                                   int?           limit     = null,
@@ -41,6 +45,7 @@ namespace Bede.Thallium.Polly
             return new CircuitBreakHandler<E>(@this, predicate, limit ?? Five, rest ?? OneMinute);
         }
 
+        [Obsolete]
         public static RetryHandler<Exception> RetryOnUnknown(this Handler @this,
                                                                   int?     attempts = null,
                                                                   Backoff  backoff  = null)
@@ -48,6 +53,7 @@ namespace Bede.Thallium.Polly
             return WaitAndRetry<Exception>(@this, IsUnknown, attempts, backoff);
         }
 
+        [Obsolete]
         public static CircuitBreakHandler<Exception> BreakOnUnknown(this Handler   @this,
                                                                          int?       limit = null,
                                                                          TimeSpan?  rest  = null)
@@ -55,6 +61,7 @@ namespace Bede.Thallium.Polly
             return CircuitBreak<Exception>(@this, IsUnknown, limit, rest);
         }
 
+        [Obsolete]
         public static RetryHandler<HttpRequestException> RetryOnServerError(this Handler @this,
                                                                                  int?     attempts = null,
                                                                                  Backoff  backoff  = null)
@@ -62,11 +69,27 @@ namespace Bede.Thallium.Polly
             return WaitAndRetry<HttpRequestException>(@this, IsServer, attempts, backoff);
         }
 
+        [Obsolete]
         public static CircuitBreakHandler<HttpRequestException> BreakOnClientError(this Handler   @this,
                                                                                         int?       limit = null,
                                                                                         TimeSpan?  rest  = null)
         {
             return CircuitBreak<HttpRequestException>(@this, IsClient, limit, rest);
+        }
+
+        public static ResponseHandler On(this ResponseHandler responseHandler, params HttpStatusCode[] codes)
+        {
+            return responseHandler.On(x => codes.Contains(x.StatusCode));
+        }
+
+        public static RetryHandler Retry(this Handler first, int? count = null, Backoff backoff = null)
+        {
+            return new RetryHandler(first, count ?? Three, backoff ?? Exponential);
+        }
+
+        public static CircuitBreakHandler Break(this Handler first, int? limit = null, TimeSpan? rest = null)
+        {
+            return new CircuitBreakHandler(first, limit ?? Five, rest ?? OneMinute);
         }
     }
 }
---
+++ Bede.Thallium.Polly/RetryHandler.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Polly;
+
+namespace Bede.Thallium.Polly
+{
+    public sealed class RetryArgs : EventArgs
+    {
+        public HttpResponseMessage Message;
+        public Exception           Last;
+        public TimeSpan            Wait;
+    }
+
+    [Obsolete]
+    public class RetryHandler<E> : PolicyHandler where E : Exception
+    {
+        public RetryHandler(HttpMessageHandler inner, Func<E, bool> predicate, int attempts, Func<int, TimeSpan> backoff)
+            : base(inner)
+        {
+            Policy = Policy.Handle(predicate).WaitAndRetryAsync(attempts, backoff, OnRetry);
+        }
+
+        public event EventHandler<RetryArgs> Retry;
+
+        Task OnRetry(Exception exception, TimeSpan next)
+        {
+            Retry?.Invoke(this, new RetryArgs { Last = exception, Wait = next });
+
+            return Task.FromResult(true);
+        }
+    }
+
+    public class RetryHandler : ResponseHandler
+    {
+        public RetryHandler(HttpMessageHandler inner, int count, Func<int, TimeSpan> backoff) : base(inner)
+        {
+            Policy = Builder.WaitAndRetryAsync(count, backoff, OnRetry);
+        }
+
+        public event EventHandler<RetryArgs> Retry;
+
+        Task OnRetry(DelegateResult<HttpResponseMessage> result, TimeSpan current)
+        {
+            Retry?.Invoke(this, new RetryArgs { Message = result.Result, Last = result.Exception, Wait = current });
+            return Task.FromResult(false);
+        }
+    }
+}
