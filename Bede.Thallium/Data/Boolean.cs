d47491628d9ddecd061c8a873f311373c2d2387a
Nicholas Little <nicholas.little@bedegaming.com>
2016-11-12 11:10:10 +0000

    thallium: Boolean and DateTime formatting
    This commit adds three simple types for handling formatting:
    
    - Boolean;
    - DateTimeFormat, taking an offset and format string, overriding
      ToString;
    - Iso, subclass providing the format string "o".
    
    DateTimeFormat is not meant to be used directly - subclass it and
    specify the format string in your domain.

---
+++ Bede.Thallium/Data/Boolean.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable 1591
+
+namespace Bede.Thallium.Data
+{
+    /// <summary>
+    /// A <see cref="bool"/> wrapper type with lowercase formatting
+    /// </summary>
+    public sealed class Boolean
+    {
+        readonly bool? _b;
+
+        Boolean(bool? b) { _b = b; }
+
+        public override string ToString()
+        {
+            return _b.HasValue ? _b.Value ? "true" : "false" : string.Empty;
+        }
+
+        public static implicit operator Boolean(bool? value) => new Boolean(value);
+    }
+}
---
+++ Bede.Thallium/Data/DateTime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+#pragma warning disable 1591
+
+namespace Bede.Thallium.Data
+{
+    /// <summary>
+    /// Base class for pre-specified <see cref="DateTimeOffset" /> formats
+    /// </summary>
+    public class DateTimeFormat
+    {
+        readonly DateTimeOffset? _time;
+        readonly string          _fmt;
+
+        public DateTimeFormat(DateTimeOffset? time, string format)
+        {
+            _time = time;
+            _fmt  = format;
+        }
+
+        public sealed override string ToString()
+        {
+            return _time?.ToString(_fmt) ?? string.Empty;
+        }
+    }
+
+    /// <summary>
+    /// Round robin format, &quot;o&quot;
+    /// </summary>
+    public sealed class Iso : DateTimeFormat
+    {
+        Iso(DateTimeOffset? time) : base(time, "o") { }
+
+        public static implicit operator Iso(DateTimeOffset? value) => new Iso(value);
+
+        public static implicit operator Iso(DateTime? value)       => new Iso(value);
+    }
+}
