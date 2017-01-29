1073f26e8b093c1b7b809000a318225e2799163c
Nicholas Little <nicholas.little@bedegaming.com>
2017-01-29 14:52:46 +0000

    thallium: Tests, add template performance test

--- Tests/Bede.Thallium.UnitTests/Standards.cs
+++ Tests/Bede.Thallium.UnitTests/Standards.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace Bede.Thallium.UnitTests
 {
@@ -7,7 +8,7 @@ namespace Bede.Thallium.UnitTests
     [TestFixture]
     class Standards
     {
-        static readonly Dictionary<string, object> Params = new Dictionary<string, object>
+        internal static readonly Dictionary<string, object> Params = new Dictionary<string, object>
         {
             { "count",      new [] { "one", "two", "three" } },
             { "dom",        new [] { "example", "com" } },
@@ -28,108 +29,113 @@ namespace Bede.Thallium.UnitTests
             { "undef",      null },
         };
 
-        [TestCase("{count}"     ,"one,two,three")]
-        [TestCase("{count*}"    ,"one,two,three")]
-        [TestCase("{/count}"    ,"/one,two,three")]
-        [TestCase("{/count*}"   ,"/one/two/three")]
-        [TestCase("{;count}"    ,";count=one,two,three")]
-        [TestCase("{;count*}"   ,";count=one;count=two;count=three")]
-        [TestCase("{?count}"    ,"?count=one,two,three")]
-        [TestCase("{?count*}"   ,"?count=one&count=two&count=three")]
-        [TestCase("{&count*}"   ,"&count=one&count=two&count=three")]
-        [TestCase("{var}"              ,"value")]
-        [TestCase("{hello}"            ,"Hello%20World%21")]
-        [TestCase("{half}"             ,"50%25")]
-        [TestCase("O{empty}X"          ,"OX")]
-        [TestCase("O{undef}X"          ,"OX")]
-        [TestCase("{x,y}"              ,"1024,768")]
-        [TestCase("{x,hello,y}"        ,"1024,Hello%20World%21,768")]
-        [TestCase("?{x,empty}"         ,"?1024,")]
-        [TestCase("?{x,undef}"         ,"?1024")]
-        [TestCase("?{undef,y}"         ,"?768")]
-        [TestCase("{var:3}"            ,"val")]
-        [TestCase("{var:30}"           ,"value")]
-        [TestCase("{list}"             ,"red,green,blue")]
-        [TestCase("{list*}"            ,"red,green,blue")]
-        [TestCase("{keys}"             ,"semi,%3B,dot,.,comma,%2C")]
-        [TestCase("{keys*}"            ,"semi=%3B,dot=.,comma=%2C")]
-        [TestCase("{+var}"                ,"value")]
-        [TestCase("{+hello}"              ,"Hello%20World!")]
-        [TestCase("{+half}"               ,"50%25")]
-        [TestCase("{base}index"           ,"http%3A%2F%2Fexample.com%2Fhome%2Findex")]
-        [TestCase("{+base}index"          ,"http://example.com/home/index")]
-        [TestCase("O{+empty}X"            ,"OX")]
-        [TestCase("O{+undef}X"            ,"OX")]
-        [TestCase("{+path}/here"          ,"/foo/bar/here")]
-        [TestCase("here?ref={+path}"      ,"here?ref=/foo/bar")]
-        [TestCase("up{+path}{var}/here"   ,"up/foo/barvalue/here")]
-        [TestCase("{+x,hello,y}"          ,"1024,Hello%20World!,768")]
-        [TestCase("{+path,x}/here"        ,"/foo/bar,1024/here")]
-        [TestCase("{+path:6}/here"        ,"/foo/b/here")]
-        [TestCase("{+list}"               ,"red,green,blue")]
-        [TestCase("{+list*}"              ,"red,green,blue")]
-        [TestCase("{+keys}"               ,"semi,;,dot,.,comma,,")]
-        [TestCase("{+keys*}"              ,"semi=;,dot=.,comma=,")]
-        [TestCase("{#var}"             ,"#value")]
-        [TestCase("{#hello}"           ,"#Hello%20World!")]
-        [TestCase("{#half}"            ,"#50%25")]
-        [TestCase("foo{#empty}"        ,"foo#")]
-        [TestCase("foo{#undef}"        ,"foo")]
-        [TestCase("{#x,hello,y}"       ,"#1024,Hello%20World!,768")]
-        [TestCase("{#path,x}/here"     ,"#/foo/bar,1024/here")]
-        [TestCase("{#path:6}/here"     ,"#/foo/b/here")]
-        [TestCase("{#list}"            ,"#red,green,blue")]
-        [TestCase("{#list*}"           ,"#red,green,blue")]
-        [TestCase("{#keys}"            ,"#semi,;,dot,.,comma,,")]
-        [TestCase("{#keys*}"           ,"#semi=;,dot=.,comma=,")]
-        [TestCase("{.who}"             ,".fred")]
-        [TestCase("{.who,who}"         ,".fred.fred")]
-        [TestCase("{.half,who}"        ,".50%25.fred")]
-        [TestCase("www{.dom*}"         ,"www.example.com")]
-        [TestCase("X{.var}"            ,"X.value")]
-        [TestCase("X{.empty}"          ,"X.")]
-        [TestCase("X{.undef}"          ,"X")]
-        [TestCase("X{.var:3}"          ,"X.val")]
-        [TestCase("X{.list}"           ,"X.red,green,blue")]
-        [TestCase("X{.list*}"          ,"X.red.green.blue")]
-        [TestCase("X{.keys}"           ,"X.semi,%3B,dot,.,comma,%2C")]
-        [TestCase("X{.keys*}"          ,"X.semi=%3B.dot=..comma=%2C")]
-        [TestCase("X{.empty_keys}"     ,"X")]
-        [TestCase("X{.empty_keys*}"    ,"X")]
-        [TestCase("{;who}"             ,";who=fred")]
-        [TestCase("{;half}"            ,";half=50%25")]
-        [TestCase("{;empty}"           ,";empty")]
-        [TestCase("{;v,empty,who}"     ,";v=6;empty;who=fred")]
-        [TestCase("{;v,bar,who}"       ,";v=6;who=fred")]
-        [TestCase("{;x,y}"             ,";x=1024;y=768")]
-        [TestCase("{;x,y,empty}"       ,";x=1024;y=768;empty")]
-        [TestCase("{;x,y,undef}"       ,";x=1024;y=768")]
-        [TestCase("{;hello:5}"         ,";hello=Hello")]
-        [TestCase("{;list}"            ,";list=red,green,blue")]
-        [TestCase("{;list*}"           ,";list=red;list=green;list=blue")]
-        [TestCase("{;keys}"            ,";keys=semi,%3B,dot,.,comma,%2C")]
-        [TestCase("{;keys*}"           ,";semi=%3B;dot=.;comma=%2C")]
-        [TestCase("{?who}"             ,"?who=fred")]
-        [TestCase("{?half}"            ,"?half=50%25")]
-        [TestCase("{?x,y}"             ,"?x=1024&y=768")]
-        [TestCase("{?x,y,empty}"       ,"?x=1024&y=768&empty=")]
-        [TestCase("{?x,y,undef}"       ,"?x=1024&y=768")]
-        [TestCase("{?var:3}"           ,"?var=val")]
-        [TestCase("{?list}"            ,"?list=red,green,blue")]
-        [TestCase("{?list*}"           ,"?list=red&list=green&list=blue")]
-        [TestCase("{?keys}"            ,"?keys=semi,%3B,dot,.,comma,%2C")]
-        [TestCase("{?keys*}"           ,"?semi=%3B&dot=.&comma=%2C")]
-        [TestCase("{&who}"             ,"&who=fred")]
-        [TestCase("{&half}"            ,"&half=50%25")]
-        [TestCase("?fixed=yes{&x}"     ,"?fixed=yes&x=1024")]
-        [TestCase("{&x,y,empty}"       ,"&x=1024&y=768&empty=")]
-        [TestCase("{&x,y,undef}"       ,"&x=1024&y=768")]
-        [TestCase("{&var:3}"           ,"&var=val")]
-        [TestCase("{&list}"            ,"&list=red,green,blue")]
-        [TestCase("{&list*}"           ,"&list=red&list=green&list=blue")]
-        [TestCase("{&keys}"            ,"&keys=semi,%3B,dot,.,comma,%2C")]
-        [TestCase("{&keys*}"           ,"&semi=%3B&dot=.&comma=%2C")]
-        public void TemplateHandling(string template, string expected)
+        internal static IEnumerable Cases()
+        {
+            yield return new TestCaseData("{count}", "one,two,three");
+            yield return new TestCaseData("{count*}", "one,two,three");
+            yield return new TestCaseData("{/count}", "/one,two,three");
+            yield return new TestCaseData("{/count*}", "/one/two/three");
+            yield return new TestCaseData("{;count}", ";count=one,two,three");
+            yield return new TestCaseData("{;count*}", ";count=one;count=two;count=three");
+            yield return new TestCaseData("{?count}", "?count=one,two,three");
+            yield return new TestCaseData("{?count*}", "?count=one&count=two&count=three");
+            yield return new TestCaseData("{&count*}", "&count=one&count=two&count=three");
+            yield return new TestCaseData("{var}", "value");
+            yield return new TestCaseData("{hello}", "Hello%20World%21");
+            yield return new TestCaseData("{half}", "50%25");
+            yield return new TestCaseData("O{empty}X", "OX");
+            yield return new TestCaseData("O{undef}X", "OX");
+            yield return new TestCaseData("{x,y}", "1024,768");
+            yield return new TestCaseData("{x,hello,y}", "1024,Hello%20World%21,768");
+            yield return new TestCaseData("?{x,empty}", "?1024,");
+            yield return new TestCaseData("?{x,undef}", "?1024");
+            yield return new TestCaseData("?{undef,y}", "?768");
+            yield return new TestCaseData("{var:3}", "val");
+            yield return new TestCaseData("{var:30}", "value");
+            yield return new TestCaseData("{list}", "red,green,blue");
+            yield return new TestCaseData("{list*}", "red,green,blue");
+            yield return new TestCaseData("{keys}", "semi,%3B,dot,.,comma,%2C");
+            yield return new TestCaseData("{keys*}", "semi=%3B,dot=.,comma=%2C");
+            yield return new TestCaseData("{+var}", "value");
+            yield return new TestCaseData("{+hello}", "Hello%20World!");
+            yield return new TestCaseData("{+half}", "50%25");
+            yield return new TestCaseData("{base}index", "http%3A%2F%2Fexample.com%2Fhome%2Findex");
+            yield return new TestCaseData("{+base}index", "http://example.com/home/index");
+            yield return new TestCaseData("O{+empty}X", "OX");
+            yield return new TestCaseData("O{+undef}X", "OX");
+            yield return new TestCaseData("{+path}/here", "/foo/bar/here");
+            yield return new TestCaseData("here?ref={+path}", "here?ref=/foo/bar");
+            yield return new TestCaseData("up{+path}{var}/here", "up/foo/barvalue/here");
+            yield return new TestCaseData("{+x,hello,y}", "1024,Hello%20World!,768");
+            yield return new TestCaseData("{+path,x}/here", "/foo/bar,1024/here");
+            yield return new TestCaseData("{+path:6}/here", "/foo/b/here");
+            yield return new TestCaseData("{+list}", "red,green,blue");
+            yield return new TestCaseData("{+list*}", "red,green,blue");
+            yield return new TestCaseData("{+keys}", "semi,;,dot,.,comma,,");
+            yield return new TestCaseData("{+keys*}", "semi=;,dot=.,comma=,");
+            yield return new TestCaseData("{#var}", "#value");
+            yield return new TestCaseData("{#hello}", "#Hello%20World!");
+            yield return new TestCaseData("{#half}", "#50%25");
+            yield return new TestCaseData("foo{#empty}", "foo#");
+            yield return new TestCaseData("foo{#undef}", "foo");
+            yield return new TestCaseData("{#x,hello,y}", "#1024,Hello%20World!,768");
+            yield return new TestCaseData("{#path,x}/here", "#/foo/bar,1024/here");
+            yield return new TestCaseData("{#path:6}/here", "#/foo/b/here");
+            yield return new TestCaseData("{#list}", "#red,green,blue");
+            yield return new TestCaseData("{#list*}", "#red,green,blue");
+            yield return new TestCaseData("{#keys}", "#semi,;,dot,.,comma,,");
+            yield return new TestCaseData("{#keys*}", "#semi=;,dot=.,comma=,");
+            yield return new TestCaseData("{.who}", ".fred");
+            yield return new TestCaseData("{.who,who}", ".fred.fred");
+            yield return new TestCaseData("{.half,who}", ".50%25.fred");
+            yield return new TestCaseData("www{.dom*}", "www.example.com");
+            yield return new TestCaseData("X{.var}", "X.value");
+            yield return new TestCaseData("X{.empty}", "X.");
+            yield return new TestCaseData("X{.undef}", "X");
+            yield return new TestCaseData("X{.var:3}", "X.val");
+            yield return new TestCaseData("X{.list}", "X.red,green,blue");
+            yield return new TestCaseData("X{.list*}", "X.red.green.blue");
+            yield return new TestCaseData("X{.keys}", "X.semi,%3B,dot,.,comma,%2C");
+            yield return new TestCaseData("X{.keys*}", "X.semi=%3B.dot=..comma=%2C");
+            yield return new TestCaseData("X{.empty_keys}", "X");
+            yield return new TestCaseData("X{.empty_keys*}", "X");
+            yield return new TestCaseData("{;who}", ";who=fred");
+            yield return new TestCaseData("{;half}", ";half=50%25");
+            yield return new TestCaseData("{;empty}", ";empty");
+            yield return new TestCaseData("{;v,empty,who}", ";v=6;empty;who=fred");
+            yield return new TestCaseData("{;v,bar,who}", ";v=6;who=fred");
+            yield return new TestCaseData("{;x,y}", ";x=1024;y=768");
+            yield return new TestCaseData("{;x,y,empty}", ";x=1024;y=768;empty");
+            yield return new TestCaseData("{;x,y,undef}", ";x=1024;y=768");
+            yield return new TestCaseData("{;hello:5}", ";hello=Hello");
+            yield return new TestCaseData("{;list}", ";list=red,green,blue");
+            yield return new TestCaseData("{;list*}", ";list=red;list=green;list=blue");
+            yield return new TestCaseData("{;keys}", ";keys=semi,%3B,dot,.,comma,%2C");
+            yield return new TestCaseData("{;keys*}", ";semi=%3B;dot=.;comma=%2C");
+            yield return new TestCaseData("{?who}", "?who=fred");
+            yield return new TestCaseData("{?half}", "?half=50%25");
+            yield return new TestCaseData("{?x,y}", "?x=1024&y=768");
+            yield return new TestCaseData("{?x,y,empty}", "?x=1024&y=768&empty=");
+            yield return new TestCaseData("{?x,y,undef}", "?x=1024&y=768");
+            yield return new TestCaseData("{?var:3}", "?var=val");
+            yield return new TestCaseData("{?list}", "?list=red,green,blue");
+            yield return new TestCaseData("{?list*}", "?list=red&list=green&list=blue");
+            yield return new TestCaseData("{?keys}", "?keys=semi,%3B,dot,.,comma,%2C");
+            yield return new TestCaseData("{?keys*}", "?semi=%3B&dot=.&comma=%2C");
+            yield return new TestCaseData("{&who}", "&who=fred");
+            yield return new TestCaseData("{&half}", "&half=50%25");
+            yield return new TestCaseData("?fixed=yes{&x}", "?fixed=yes&x=1024");
+            yield return new TestCaseData("{&x,y,empty}", "&x=1024&y=768&empty=");
+            yield return new TestCaseData("{&x,y,undef}", "&x=1024&y=768");
+            yield return new TestCaseData("{&var:3}", "&var=val");
+            yield return new TestCaseData("{&list}", "&list=red,green,blue");
+            yield return new TestCaseData("{&list*}", "&list=red&list=green&list=blue");
+            yield return new TestCaseData("{&keys}", "&keys=semi,%3B,dot,.,comma,%2C");
+            yield return new TestCaseData("{&keys*}", "&semi=%3B&dot=.&comma=%2C");
+        }
+
+        [TestCaseSource(nameof(Cases))]
+        public void Test(string template, string expected)
         {
             var sut = new Rfc6570();
 
