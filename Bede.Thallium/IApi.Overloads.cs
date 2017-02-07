21669f4908d6d02ee7c7e420befc5a369219b19b
Nicholas Little <nicholas.little@bedegaming.com>
2017-02-07 10:37:53 +0000

    thallium: Restore old construction overloads
    In commit 6c0a7f9 we mentioned that the change will break ABI
    compatibility and require a recompile. We add the old signatures to a
    new file which we tuck away as a dependent.

---
+++ Bede.Thallium/IApi.Overloads.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+#pragma warning disable 618, 1591
+
+namespace Bede.Thallium
+{
+    using Clients;
+
+    using Handler    = HttpMessageHandler;
+    using Formatters = MediaTypeFormatterCollection;
+
+    public static partial class ApiExtensions
+    {
+        public static T New<T>(this IApi<RestClient> emitter, Uri        uri,
+                                                              Handler    handler,
+                                                              Formatters formatters)
+        {
+            return emitter.New<T>(new object[] { uri, handler, formatters });
+        }
+
+        public static RestClient New(this IApi<RestClient> emitter, Type       @interface,
+                                                                    Uri         uri,
+                                                                    Handler     handler,
+                                                                    Formatters  formatters)
+        {
+            return emitter.New(@interface, new object[] { uri, handler, formatters });
+        }
+    }
+}
