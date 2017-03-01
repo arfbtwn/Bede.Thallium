c001755ab1d5637f5c2439504eade3c2eaa9f2b0
Nicholas Little <nicholas.little@bedegaming.com>
2017-03-01 10:16:29 +0000

    thallium: Testing, test the testing library

---
+++ Tests/Bede.Thallium.UnitTests/Mocking.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Bede.Thallium.UnitTests
+{
+    using Testing;
+
+    [TestFixture]
+    public class Mocking
+    {
+        [Test]
+        public void Throws()
+        {
+            var sut = new Apis<IApi>()
+                .Cases(x => x.Foo())
+                    .Good(HttpStatusCode.OK)
+                    .Bad (HttpStatusCode.NotFound)
+                    .Ugly(HttpStatusCode.InternalServerError)
+                    .Build()
+                .Cases(x => x.Bar())
+                    .Good(HttpStatusCode.OK)
+                    .Bad (HttpStatusCode.NotFound)
+                    .Ugly(HttpStatusCode.InternalServerError)
+                    .Build();
+
+            Assert.DoesNotThrowAsync(async () => await sut.Good.Foo());
+            Assert.DoesNotThrowAsync(async () => await sut.Good.Bar());
+
+            Assert.ThrowsAsync<HttpRequestException>(async () => await sut.Bad.Foo());
+            Assert.ThrowsAsync<HttpRequestException>(async () => await sut.Bad.Bar());
+
+            Assert.ThrowsAsync<HttpRequestException>(async () => await sut.Ugly.Foo());
+            Assert.ThrowsAsync<HttpRequestException>(async () => await sut.Ugly.Bar());
+        }
+
+        public interface IApi
+        {
+            Task<object> Foo();
+            Task         Bar();
+        }
+    }
+}
