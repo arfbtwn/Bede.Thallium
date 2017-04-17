cfe49021c7b9b7d2a267563e577f9481b8f7bfbb
Nicholas Little <nicholas.little@bedegaming.com>
2017-04-17 16:46:23 +0100

    thallium: Templating perf, Ignore => Explict
    These attributes behave differently in VS - Ignore doesn't run whether a
    TestFixture is selected or not from the unit runner window. Suspect this
    is something to do with the tests inheriting the ignored state from the
    fixture and refusing to run. Anyway, we can run them without source
    changes now.

--- Tests/Bede.Thallium.UnitTests/Performance.cs
+++ Tests/Bede.Thallium.UnitTests/Performance.cs
@@ -5,7 +5,7 @@ using NUnit.Framework;
 
 namespace Bede.Thallium.UnitTests
 {
-    [Ignore("Run manually")]
+    [Explicit]
     [TestFixture(1e3, 50)]
     [TestFixture(1e6, 10000)]
     class Performance
