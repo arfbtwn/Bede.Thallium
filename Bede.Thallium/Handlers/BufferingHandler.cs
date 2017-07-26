d205ab0f7d1cf3f6ad55e57d7eda2706fac0780c
Nicholas Little <nicholas.little@bedegaming.com>
2017-07-26 18:47:23 +0100

    thallium: Add BufferingHandler
    A simple handler that buffers its content. The Throw handler already
    does this under error conditions but this will do it for both in and
    output - handy for logging but use with care.

---
+++ Bede.Thallium/Handlers/BufferingHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+#pragma warning disable 1591
+
+namespace Bede.Thallium.Handlers
+{
+    using Belt;
+
+    /// <summary>
+    /// A simple handler that buffers its input and output content
+    /// </summary>
+    public sealed class BufferingHandler : DelegatingHandler
+    {
+        public BufferingHandler(HttpMessageHandler inner) : base(inner) { }
+        public BufferingHandler() : this(new HttpClientHandler()) { }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            await request.Content.LoadIntoBufferAsync().Caf();
+
+            var msg = await base.SendAsync(request, cancellationToken).Caf();
+
+            await msg.Content.LoadIntoBufferAsync().Caf();
+
+            return msg;
+        }
+    }
+}
--- Tests/Bede.Thallium.UnitTests/Handlers.cs
+++ Tests/Bede.Thallium.UnitTests/Handlers.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Bede.Thallium.Handlers;
@@ -69,6 +72,32 @@ namespace Bede.Thallium.UnitTests
 
             Assert.AreEqual((tries + 1) * limit, count);
             Assert.IsTrue(broken);
+        }
+
+        [Test]
+        public void Buffering()
+        {
+            var sm = new HttpRequestMessage
+            {
+                RequestUri = new Uri("http://localhost"),
+                Content = new StreamContent(new MemoryStream(Encoding.UTF8.GetBytes("1")))
+            };
+            var rm = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StreamContent(new MemoryStream(Encoding.UTF8.GetBytes("2")))
+            };
+            var mock = new Mock<HttpMessageHandler>();
+
+            mock.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .Returns(Task.FromResult(rm));
+
+            var sut = new HttpClient(new BufferingHandler(mock.Object));
+
+            var result = sut.SendAsync(sm).Result;
+
+            Assert.IsNotNull(result);
         }
     }
 }
